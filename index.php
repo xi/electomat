437a76fbf43d940594f9246dc399200059daa5b5
Tobias Bengfort <tobias.bengfort@gmx.net>
2014-04-17 11:34:56 +0200

    add flexible php example

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+if (!function_exists('http_negotiate_language')) {
+	function http_negotiate_language($supported) {
+		return $supported[0];
+	}
+}
+?><!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<script type="text/javascript" src="electomat.js"></script>
+	<link rel="stylesheet" type="text/css" href="electomat.css"/>
+</head>
+
+<body>
+	<div class="electomat" src="<?=$_GET['json'] ? $_GET['json'] : 'example.json'?>" lang="<?=http_negotiate_language(array('en', 'de'))?>"></div>
+</body>
+</html>
