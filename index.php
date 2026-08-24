920fefff0eb11924939c198b0f5cc9d14f3d8283
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-08-24 23:15:17 +0200

    tabs to spaces

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 if (!function_exists('http_negotiate_language')) {
-	function http_negotiate_language($supported) {
-		return $supported[0];
-	}
+    function http_negotiate_language($supported) {
+        return $supported[0];
+    }
 }
 ?><!DOCTYPE html>
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<script type="text/javascript" src="electomat.js"></script>
-	<link rel="stylesheet" type="text/css" href="electomat.css"/>
-	<style>
-		body {
-			margin: 0;
-			height: 100vh;
-		}
-	</style>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="electomat.js"></script>
+    <link rel="stylesheet" type="text/css" href="electomat.css"/>
+    <style>
+        body {
+            margin: 0;
+            height: 100vh;
+        }
+    </style>
 </head>
 
 <body>
-	<div class="electomat" src="<?=$_GET['json'] ? $_GET['json'] : 'example.json'?>" lang="<?=http_negotiate_language(array('en', 'de'))?>"></div>
+    <div class="electomat" src="<?=$_GET['json'] ? $_GET['json'] : 'example.json'?>" lang="<?=http_negotiate_language(array('en', 'de'))?>"></div>
 </body>
 </html>
